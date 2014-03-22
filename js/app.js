abcf2d6a594fc6ea13bee1605181896bc15c826e
Evan Lloyd <eulloyd@gmail.com>
2014-03-23 00:04:21 +0900

    Refactor for functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,43 @@
 $(document).ready(function(){
-	for(var i = 1; i <101; i++)
-		if(i % 15 === 0)
-			$('body').append('<p>FizzBuzz</p>')
-		else if(i % 3 === 0)
-			$('body').append('<p>Fizz</p>')
-		else if(i % 5 === 0)
-		    $('body').append('<p>Buzz</p>')
-		else
-			$('body').append('<p>'+i+'</p>')
-})
+
+	var fizzBuzz = function (limit) {
+		console.log(limit)
+		for (var num = 1; num < limit; num++) 
+		{
+			if (num % 15 === 0) {
+				$('body').append("<p>FizzBuzz</p>")
+			}
+			else if (num % 3 === 0) {
+				$('body').append("<p>Fizz</p>")
+			}
+			else if (num % 5 === 0) {
+				$('body').append("<p>Buzz</p>")
+			}
+			else {
+				$('body').append("<p>"+num+"</p>")
+			}
+			
+		}
+	}
+
+	var tryAgain = function() {
+		alert("That's not a whole number");
+	}
+
+    var loop = function () {
+    	var startingNumber = prompt("Enter any whole number");
+    	if (isNaN(startingNumber)) {
+    		tryAgain()
+    		loop()
+    	}
+    	else if (+startingNumber % 1 != 0) {
+    		tryAgain()
+    		loop()
+    	}
+    	else {
+    		fizzBuzz(+startingNumber)
+    	}
+	}
+
+	loop()
+});
