a09b113d17da41ca40c8d56a4d1e6ef738637098
Evan Lloyd <eulloyd@gmail.com>
2014-03-24 21:18:15 +0900

    fix js format errors

--- js/app.js
+++ js/app.js
@@ -1,43 +1,42 @@
 $(document).ready(function(){
 
 	var fizzBuzz = function (limit) {
-		console.log(limit)
-		for (var num = 1; num < limit; num++) 
-		{
+		console.log(limit);
+		for (var num = 1; num < limit; num++) {
 			if (num % 15 === 0) {
-				$('body').append("<p>FizzBuzz</p>")
+				$('body').append("<p>FizzBuzz</p>");
 			}
 			else if (num % 3 === 0) {
-				$('body').append("<p>Fizz</p>")
+				$('body').append("<p>Fizz</p>");
 			}
 			else if (num % 5 === 0) {
-				$('body').append("<p>Buzz</p>")
+				$('body').append("<p>Buzz</p>");
 			}
 			else {
-				$('body').append("<p>"+num+"</p>")
+				$('body').append("<p>"+num+"</p>");
 			}
 			
 		}
-	}
+	};
 
 	var tryAgain = function() {
 		alert("That's not a whole number");
-	}
+	};
 
     var loop = function () {
-    	var startingNumber = prompt("Enter any whole number");
-    	if (isNaN(startingNumber)) {
-    		tryAgain()
-    		loop()
-    	}
-    	else if (+startingNumber % 1 != 0) {
-    		tryAgain()
-    		loop()
-    	}
-    	else {
-    		fizzBuzz(+startingNumber)
-    	}
-	}
+      var startingNumber = prompt("Enter any whole number");
+      if (isNaN(startingNumber)) {
+        tryAgain();
+        loop();
+      }
+      else if (+startingNumber % 1 !== 0) {
+        tryAgain();
+        loop();
+      }
+        else {
+          fizzBuzz(+startingNumber);
+        }
+	};
 
-	loop()
+	loop();
 });
